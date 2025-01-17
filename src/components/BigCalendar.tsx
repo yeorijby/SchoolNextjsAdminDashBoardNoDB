b7d391cf9abb62002dfdfc08923a221bf0a38b67
칼있으마 <bok-yeol.jung@doosan.com>
2025-01-17 16:01:57 +0900

    1:32:35 달력 거시기하는중

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -3,24 +3,41 @@
 "use client"
 
 
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
 import moment from 'moment'
 import { calendarEvents } from '@/lib/data'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { useState } from 'react'
 
 const localizer = momentLocalizer(moment)
 
+const START_TIME=8;
+const END_TIME=17;
+
+
 const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+  const [curDT, SetCurDT] = useState<Date>(new Date());
+  const [curYear, SetCurYear] = useState(curDT.getFullYear());
+  const [curMonth, SetCurMonth] = useState(curDT.getMonth());
+
+  const handleOnChangeView = (selectedView: View) => {
+    setView(selectedView);
+  }
+
   return (
-    <div className=''>
-        <Calendar
-            localizer={localizer}
-            events={calendarEvents}
-            startAccessor="start"
-            endAccessor="end"
-            style={{ height: 500 }}
-        />
-    </div>
+    <Calendar
+      localizer={localizer}
+      events={calendarEvents}
+      startAccessor="start"
+      endAccessor="end"
+      views={["work_week", "day"]}
+      view={view}
+      style={{ height: "98%" }}
+      onView={handleOnChangeView}
+      min={new Date(curYear,curMonth,0,START_TIME,0,0)}
+      max={new Date(curYear,curMonth,0,END_TIME,0,0)}
+    />
   )
 }
 
